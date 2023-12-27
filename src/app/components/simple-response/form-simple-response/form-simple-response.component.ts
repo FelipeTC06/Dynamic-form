5f4c59792d7c4feb02fc97f9872352980a029a05
Felipe Torres Cresto <felipe.t.cresto@gmail.com>
2023-12-27 02:35:24 -0300

    feat: creat simple response form

--- src/app/components/simple-response/form-simple-response/form-simple-response.component.ts
+++ src/app/components/simple-response/form-simple-response/form-simple-response.component.ts
@@ -1,12 +1,100 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-simple-response',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form-simple-response.component.html',
   styleUrl: './form-simple-response.component.scss'
 })
 export class FormSimpleResponseComponent {
+
+  public simpleResponseForm!: FormGroup;
+
+  public fields: any[] = [
+    {
+      type: 'separator',
+      label: 'Personal Information',
+    },
+    {
+      type: 'text',
+      label: 'First Name',
+      name: 'firstName',
+    },
+    {
+      type: 'text',
+      label: 'Last Name',
+      name: 'lastName',
+    },
+    {
+      type: 'radio',
+      label: 'Gender',
+      name: 'gender',
+      options: [
+        { value: 'M', label: 'Male' },
+        { value: 'F', label: 'Female' }
+      ]
+    },
+    {
+      type: 'checkbox',
+      label: 'Do you like sports?',
+      name: 'likeSports',
+    },
+    {
+      type: 'select',
+      label: 'Favorite sport?',
+      name: 'favSport',
+      options: [
+        { value: 'SU', label: 'Surf' },
+        { value: 'SO', label: 'Soccer' },
+        { value: 'SW', label: 'swimming' }
+      ]
+    },
+    {
+      type: 'separator',
+      label: 'Work Information',
+    },
+    {
+      type: 'checkbox',
+      label: 'Is employed?',
+      name: 'isEmployed',
+    },
+    {
+      type: 'text',
+      label: 'Company Name',
+      name: 'companyName',
+    },
+    {
+      type: 'radio',
+      label: 'Are you happy with your position?',
+      name: 'happyPosition',
+      options: [
+        { value: 'H', label: 'Happy' },
+        { value: 'S', label: 'Satisfied' },
+        { value: 'UN', label: 'Unhappy' },
+      ]
+    },
+  ]
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  public ngOnInit() {
+    this.createForm()
+  }
+
+  public createForm() {
+    this.simpleResponseForm = this.formBuilder.group({});
+    this.fields.forEach(field => {
+      if (field.type !== 'separator') {
+        this.simpleResponseForm.addControl(field.name, new FormControl(''));
+      }
+    });
+  }
+
+  public onSubmit() {
+    console.log('simpleResponseForm', this.simpleResponseForm.value);
+  }
 
 }
